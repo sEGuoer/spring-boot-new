ac949daf926a7bf5ebe18b52edaa202686ee7237
sEGuoer <709074535@qq.com>
2023-10-18 17:46:57 +0800

    检测容器中是否有对应的bean

--- src/main/java/com/seguoer/config/MyConfig.java
+++ src/main/java/com/seguoer/config/MyConfig.java
@@ -1,7 +1,9 @@
 package com.seguoer.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 @EnableConfigurationProperties(MyProperties.class)
+@Configuration
 public class MyConfig {
 }
--- src/main/java/com/seguoer/controller/helloController.java
+++ src/main/java/com/seguoer/controller/helloController.java
@@ -1,14 +1,23 @@
 package com.seguoer.controller;
 
+import com.seguoer.config.MyProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class helloController {
-
+    @Autowired
+    MyProperties myProperties;
     @RequestMapping("/")
     String home() {
         return "Hello World!";
+    }
+
+    @RequestMapping("/myProperties")
+    String myProperties() {
+        System.out.println(myProperties.getName());
+        return myProperties.getName();
     }
 
 }
