2c9d084dffea05bb630bc5987ee42c61b9c24211
sEGuoer <709074535@qq.com>
2023-10-19 11:27:34 +0800

    stater-thymeleaf引入与基本运用

--- src/main/java/com/seguoer/controller/HelloController.java
+++ src/main/java/com/seguoer/controller/HelloController.java
@@ -2,22 +2,32 @@ package com.seguoer.controller;
 
 import com.seguoer.config.MyProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class helloController {
+@Controller
+public class HelloController {
     @Autowired
     MyProperties myProperties;
     @RequestMapping("/")
+    @ResponseBody
     String home() {
         return "Hello World!";
     }
-
+    @RequestMapping("/show")
+    String show(Model model){
+        model.addAttribute("hello","hello world");
+        return "demo1";
+    }
     @RequestMapping("/myProperties")
+    @ResponseBody
     String myProperties() {
         System.out.println(myProperties.getName());
         return myProperties.getName();
     }
+
 
 }
