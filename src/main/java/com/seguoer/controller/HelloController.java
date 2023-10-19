be535d5f935cfb3c65f0f9e7fb3650cc7b61dec1
sEGuoer <709074535@qq.com>
2023-10-19 16:34:50 +0800

    本质上三种方法都是一个效果setAttribute

--- src/main/java/com/seguoer/controller/HelloController.java
+++ src/main/java/com/seguoer/controller/HelloController.java
@@ -1,12 +1,15 @@
 package com.seguoer.controller;
 
 import com.seguoer.config.MyProperties;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @Controller
 public class HelloController {
@@ -18,7 +21,9 @@ public class HelloController {
         return "Hello World!";
     }
     @RequestMapping("/show")
-    String show(Model model){
+    String show(Model model, Map<String,Object> map , HttpServletRequest req){
+        map.put("HELLO","HELLO WORLD");
+        req.setAttribute("HLO","HLO WD");
         model.addAttribute("hello","hello world");
         return "demo1";
     }
