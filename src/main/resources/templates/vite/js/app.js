8efabfed939135de21bf88f6cd2150c266cac144
sEGuoer <709074535@qq.com>
2023-10-20 09:09:08 +0800

    登陆页面基本功能

---
+++ src/main/resources/templates/vite/js/app.js
@@ -0,0 +1,11 @@
+import "../scss/app.scss"
+
+import AOS from 'aos'
+import "aos/dist/aos"
+AOS.init({
+    duration: 1000,
+});
+
+import "bootstrap/dist/js/bootstrap.min"
+
+import "admin-lte/dist/js/adminlte.min"
