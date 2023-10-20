21d657b5990d4ea45a24eeb7d05451c8b178eabf
sEGuoer <709074535@qq.com>
2023-10-20 09:09:48 +0800

    测试是否能正常拿到POST并去掉静态前缀

---
+++ src/main/java/com/seguoer/controller/OpearatingController.java
@@ -0,0 +1,17 @@
+package com.seguoer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class OpearatingController {
+    @PostMapping("/verify")
+    String verify(Model model, @RequestParam String email, @RequestParam String pwd) {
+        System.out.println(email);
+        System.out.println(pwd);
+        model.addAttribute("email",email);
+        return "demo1.html";
+    }
+}
